bafbc1bcbbfac31af1bfdf91878f7008d6f80e3e
Gabriel <capaygabriel98@gmail.com>
2024-08-28 21:14:54 -0300

    #

---
+++ src/main/java/com/example/Los_Andes/LosAndesApplication.java
@@ -0,0 +1,13 @@
+package com.example.Los_Andes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LosAndesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LosAndesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Los_Andes/LosAndesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Los_Andes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LosAndesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
